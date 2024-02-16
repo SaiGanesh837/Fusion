84e6437fb2c061cecb1f4f812e9978e5f6c64997
Chintalapalli Sai Ganesh <saiganeshchintalapalli@gmail.com>
2024-02-16 18:35:26 +0530

    added authentication model

--- FusionIIIT/applications/examination/models.py
+++ FusionIIIT/applications/examination/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+# your_app/models.py
 
-# Create your models here.
+
+
+class Authentication(models.Model):
+    authenticator_1 = models.BooleanField(default=False)
+    authenticator_2 = models.BooleanField(default=False)
+    authenticator_3 = models.BooleanField(default=False)
+    course = models.CharField(max_length=255, default="")
+
+    def __str__(self):
+        return f"{self.course} - Auth1: {self.authenticator_1}, Auth2: {self.authenticator_2}, Auth3: {self.authenticator_3}"
--- FusionIIIT/applications/examination/views.py
+++ FusionIIIT/applications/examination/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
+
 # from applications.academic_information.models import Student
 from applications.globals.models import (DepartmentInfo, Designation,
                                          ExtraInfo, Faculty, HoldsDesignation)
